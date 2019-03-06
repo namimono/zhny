aa425ed3be039c0e14ecc6b9f764a498567f45e9
xmh <xmh1010092512@vip.qq.com>
2019-03-06 17:40:35 +0800

    解决冲突

--- src/main/java/org/rcisoft/dao/sysUser/userManager/SysUserDao.java
+++ src/main/java/org/rcisoft/dao/sysUser/userManager/SysUserDao.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 import org.rcisoft.entity.SysUser;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
@@ -11,18 +12,14 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Create by 土豆儿
+ * Create by Minghui Xu
  * Time：2019/3/5 13:28
  **/
+@Component("SysUserDao2")
 @Repository
 public interface SysUserDao extends Mapper<SysUser> {
 
-    /**
-     * 获取业主信息
-     */
-    @Select("SELECT * FROM sys_user WHERE type = '3';")
-    @ResultType(SysUser.class)
-    List<Map<String,Object>> queryOwnerInfo();
+
 
     /**
      * 根据用户名查询信息
