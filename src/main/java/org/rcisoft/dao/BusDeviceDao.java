eecd5f4dd8850e9262b4099161996d73861ddf06
liuyuanjun <465255696@qq.com>
2019-03-06 18:01:52 +0800

    添加 项目维护-设备配置 添加接口

---
+++ src/main/java/org/rcisoft/dao/BusDeviceDao.java
@@ -0,0 +1,13 @@
+package org.rcisoft.dao.sysManagement.projMaintenance;
+
+import org.rcisoft.entity.BusDevice;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Create by 土豆儿
+ * Time：2019/3/6 17:22
+ **/
+@Repository
+public interface BusDeviceDao extends Mapper<BusDevice> {
+}
