3a8e6e5e53cb3df1b45a04139309b6e927489324
jichao <jichao835@sina.com>
2019-03-25 19:01:51 +0800

    能耗管理--碳排放量

---
+++ src/main/java/org/rcisoft/business/energy/emission/service/EmissionService.java
@@ -0,0 +1,29 @@
+package org.rcisoft.business.energy.emission.service;
+
+import org.rcisoft.business.energy.emission.entity.EmissionParam;
+import org.rcisoft.business.energy.emission.entity.EmissionResult;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by JiChao on 2019/3/25.
+ * 能耗管理--用能比较
+ */
+public interface EmissionService {
+
+    /**
+     * 查询碳排放量
+     * @param projectId
+     * @return
+     */
+    EmissionResult queryEmission(String projectId);
+
+    /**
+     * 碳排放强度统计
+     * @param emissionParam projectId，year，month，day
+     * @return
+     */
+    List<BigDecimal> queryEmissionStatistics(EmissionParam emissionParam);
+
+}
