68aa38a2952c9f82b0a21a099f985fd7fc2056aa
liuyuanjun <465255696@qq.com>
2019-03-26 09:55:18 +0800

    添加 电子报表-系统数据 查询二级参数接口

--- src/main/java/org/rcisoft/business/equipment/report/service/Impl/SystemDataServiceImpl.java
+++ src/main/java/org/rcisoft/business/equipment/report/service/Impl/SystemDataServiceImpl.java
@@ -2,8 +2,12 @@ package org.rcisoft.business.equipment.report.service.Impl;
 
 import org.rcisoft.business.equipment.report.dao.SystemDataDao;
 import org.rcisoft.business.equipment.report.service.SystemDataService;
+import org.rcisoft.dao.BusParamSecondDao;
+import org.rcisoft.entity.BusParamSecond;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author 土豆儿
@@ -14,4 +18,14 @@ public class SystemDataServiceImpl implements SystemDataService {
 
     @Autowired
     private SystemDataDao systemDataDao;
+    @Autowired
+    private BusParamSecondDao busParamSecondDao;
+
+    /**
+     * 根据参数来源查询二级参数
+     */
+    @Override
+    public List<BusParamSecond> queryParamSecondBySource(String proId,String sourceId){
+        return busParamSecondDao.queryParamSecondByProId(proId,sourceId);
+    }
 }
--- src/main/java/org/rcisoft/business/equipment/report/service/SystemDataService.java
+++ src/main/java/org/rcisoft/business/equipment/report/service/SystemDataService.java
@@ -1,9 +1,18 @@
 package org.rcisoft.business.equipment.report.service;
 
+import org.rcisoft.entity.BusParamSecond;
+
+import java.util.List;
+
 /**
  * @author 土豆儿
  * @date 2019/3/19 15:17
  **/
 
 public interface SystemDataService {
+
+    /**
+     * 根据参数来源查询二级参数
+     */
+    List<BusParamSecond> queryParamSecondBySource(String proId,String sourceId);
 }
