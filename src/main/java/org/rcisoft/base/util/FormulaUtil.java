ec2e70e53796dab3e4af0689243214c89dc5b9c1
jichao <jichao835@sina.com>
2019-04-04 15:22:52 +0800

    修改公式工具类

--- src/main/java/org/rcisoft/base/util/FormulaUtil.java
+++ src/main/java/org/rcisoft/base/util/FormulaUtil.java
@@ -1,7 +1,14 @@
 package org.rcisoft.base.util;
 
+import com.alibaba.fastjson.JSONObject;
 import com.greenpineyu.fel.FelEngine;
 import com.greenpineyu.fel.FelEngineImpl;
+import org.apache.commons.lang3.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author 土豆儿
@@ -15,29 +22,38 @@ public class FormulaUtil {
      * @param formula （需将公式中的变量替换为对应的数值后传入）
      * @return 表达式计算后的结果
      */
-    public static String getResult(String formula) {
-        // 公式计算类
-        FelEngine fel = new FelEngineImpl();
+    public static String calculate(String formula, FelEngine fel) {
         // 结果
-        String eval = "";
+        String eval = null;
         // 判断表达式是否由数字和运算符号组成
-        boolean flag = true;
-        String[] split = formula.split("");
-        for (String s : split) {
-            if (!s.matches("\\d|\\(|\\)|\\+|\\-|\\*|/|%|\\.")) {
-                flag = false;
-                break;
-            }
-        }
+        boolean flag = formula.matches("(\\d|\\(|\\)|\\+|\\-|\\*|/|%|\\.)+");
         // 计算出公式的最终结果
         if (flag) {
             Object result = fel.eval(formula);
             if (result != null) {
                 eval = result.toString();
             }
-        }else {
-            eval = null;
         }
         return eval;
+    }
+
+    /**
+     * 根据一级，二级code从json中获取数据
+     * @param first
+     * @param second
+     * @param json
+     * @return
+     */
+    public static String getValueFromJson(String first, String second, String json) {
+        String result = null;
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        JSONObject jsonFirst = jsonObject.getJSONObject(first);
+        if (jsonFirst != null) {
+            JSONObject regJson = jsonFirst.getJSONObject("REG_VAL");
+            if (regJson != null) {
+                result = regJson.getString(second);
+            }
+        }
+        return result;
     }
 }
