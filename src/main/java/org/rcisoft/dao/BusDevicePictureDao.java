2cc358dda92e94cfc428952d3ffd56b68cd49639
jichao <jichao835@sina.com>
2019-04-17 09:28:32 +0800

    增加dao层

---
+++ src/main/java/org/rcisoft/dao/BusDevicePictureDao.java
@@ -0,0 +1,12 @@
+package org.rcisoft.dao;
+
+import org.rcisoft.entity.BusDevicePicture;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by JiChao on 2019/4/17.
+ */
+@Repository
+public interface BusDevicePictureDao extends Mapper<BusDevicePicture> {
+}
