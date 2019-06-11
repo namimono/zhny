f22d6bdaaf8395b92cf6fb777d8232ed35ecfa6c
WangXinyu <615948603@qq.com>
2019-06-11 09:25:04 +0800

    修改了公式保存提示

--- src/main/java/org/rcisoft/business/equipment/report/controller/FormulaOperationController.java
+++ src/main/java/org/rcisoft/business/equipment/report/controller/FormulaOperationController.java
@@ -5,14 +5,11 @@ import io.swagger.annotations.ApiOperation;
 import org.rcisoft.base.result.Result;
 import org.rcisoft.business.equipment.report.entity.FormulaParams;
 import org.rcisoft.business.equipment.report.service.FormulaOperationService;
-import org.rcisoft.entity.BusFormula;
-import org.rcisoft.entity.BusVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 /**
  * @author 土豆儿
@@ -29,7 +26,7 @@ public class FormulaOperationController {
     @ApiOperation(value="编辑公式和变量",notes="需要的参数（字段名称与数据库一致） 公式：busFormula，参数集合：variableList，删除的参数ids：deleteVariableIds")
     @PostMapping("/editFormulaAndVariable")
     public Result editFormulaAndVariable(@RequestBody FormulaParams formulaParams) {
-        return Result.result(formulaOperationServiceImpl.editFormulaAndVariable(formulaParams), "编辑公式成功", "编辑公式失败");
+        return Result.result(formulaOperationServiceImpl.editFormulaAndVariable(formulaParams), "保存公式成功", "保存公式失败");
     }
 
     @ApiOperation(value="删除公式",notes="需要的参数（字段名称与数据库一致） 公式id")
