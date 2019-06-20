4d2a643385f73fd2064684df35bbe5d4c1d47e4b
WangXinyu <615948603@qq.com>
2019-06-20 15:02:40 +0800

    修改bug

--- src/main/java/org/rcisoft/dao/EnergySavePlanDao.java
+++ src/main/java/org/rcisoft/dao/EnergySavePlanDao.java
@@ -1,6 +1,7 @@
 package org.rcisoft.dao;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultType;
 import org.rcisoft.entity.EnergySavePlan;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,6 @@ public interface EnergySavePlanDao extends Mapper<EnergySavePlan> {
             "values(#{item.id}, #{item.projectId},#{item.timeMonth}, #{item.timeDay}, #{item.money}) " +
             "</foreach></script>")
     @ResultType(Integer.class)
-    int batchSave(List<EnergySavePlan> saveList);
+    int batchSave(@Param("saveList") List<EnergySavePlan> saveList);
 
 }
